11c26a39f18ba990551fd77c8119bfb3c0a1f4e6
Abdul Mutalib <trust_dboy@yahoo.com>
2017-07-18 11:46:25 +0000

    Button press and relase

--- led.py
+++ led.py
@@ -3,8 +3,38 @@ from time import sleep
 
 led = LED(17)
 
-while True:
+def dot():
+	led.on()
+	sleep(0.01)
+	led.off()
+	sleep(1)
+
+def name():
 	led.on()
 	sleep(1)
 	led.off()
-	sleep(1)
+	sleep(0.01)
+
+def dash():
+	led.on()
+	sleep(0.01)
+	led.off()
+	sleep(2)
+
+dot()
+dot()
+name()
+dash()
+dash()
+dot()
+dot()
+name()
+dash()
+dash()
+dot()
+dot()
+name()
+dash()
+dash()
+dot()
+dot()
